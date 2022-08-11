962d271cd4981ec818decf19b0a7458a93cadea6
Roxanne <35779969+CyanSorcery@users.noreply.github.com>
2022-08-10 18:00:10 -0700

    complete implementation of image compressor

--- prep_tex_sheets.php
+++ prep_tex_sheets.php
@@ -8,6 +8,44 @@ ini_set('display_errors', 1);
     as well as reduce the color count to be more easily usable by the image generation routine.
 */
 
+function tile_to_hex($image, $x_pos, $y_pos)
+{
+    //Converts a palette-based image tile to a hex string.
+    $hex_str        = '';
+    $x_max          = $x_pos + 8;
+    $y_max          = $y_pos + 8;
+
+    for ($x = $x_pos; $x < $x_max; $x++)
+        for ($y = $y_pos; $y < $y_max; $y++)
+            $hex_str    .= base_convert(imagecolorat($image, $x, $y), 10, 16);
+
+    return $hex_str;
+}
+
+function draw_hex_sprite($image, $pal, $tile_hex, $x_pos, $y_pos)
+{
+    //Convert a hex string into a sprite and draw it to the image
+    $chars          = strlen($tile_hex);
+    $x              = $x_pos;
+    $y              = $y_pos;
+    $x_max          = $x_pos + 8;
+
+    for ($i = 0; $i < $chars; $i++)
+    {
+        $ind        = base_convert(substr($tile_hex, $i, 1), 16, 10);
+        
+        if ($ind > 0)
+            imagesetpixel($image, $x, $y, $pal[$ind]);
+        $x++;
+
+        if ($x >= $x_max)
+        {
+            $x      = $x_pos;
+            $y++;
+        }
+    }
+}
+
 //Scan the directory for all texture pages
 $page_pngs      = scandir('./tex/');
 
@@ -44,7 +82,6 @@ $nes_gb_tiles           = [];
 $snes_tiles             = [];
 
 //We should now have some textures to work with, so let's work with them!
-$tmp_num        = 0;
 foreach ($tex_pages as $page)
 {
     //Get the image dimensions
@@ -77,14 +114,153 @@ foreach ($tex_pages as $page)
     imagetruecolortopalette($page, false, 3);
     imagetruecolortopalette($page_hd, false, 15);
 
-    //Temporary
-    imagepng($page, "./{$tmp_num}.png");
-    $tmp_num++;
-    imagepng($page_hd, "./{$tmp_num}.png");
-    $tmp_num++;
+    //Go over the images and convert the image data to a hex string
+    print("Converting data to NES/GameBoy tiles...\r\n");
+
+    for ($x_cell = 0; $x_cell < $page_w; $x_cell += 8)
+    {
+        for ($y_cell = 0; $y_cell < $page_h; $y_cell += 8)
+        {
+            $nes_gb_tiles[]     = tile_to_hex($page, $x_cell, $y_cell);
+        }
+
+        //Progress indicator
+        echo chr(27) . "[0G";
+        $percentage         = round(($x_cell / $page_w) * 10);
+        echo '[' . str_repeat('=', $percentage) . str_repeat(' ', 10 - $percentage) . '] ' . ($percentage * 10) . '%';
+    }
+    print("\r\n");
+
+    print("Converting data to SNES tiles...\r\n");
+
+    for ($x_cell = 0; $x_cell < $page_w; $x_cell += 8)
+    {
+        for ($y_cell = 0; $y_cell < $page_h; $y_cell += 8)
+        {
+            $snes_tiles[]     = tile_to_hex($page_hd, $x_cell, $y_cell);;
+        }
+
+        //Progress indicator
+        echo chr(27) . "[0G";
+        $percentage         = round(($x_cell / $page_w) * 10);
+        echo '[' . str_repeat('=', $percentage) . str_repeat(' ', 10 - $percentage) . '] ' . ($percentage * 10) . '%';
+    }
+    print("\r\n");
+
+    //Remove the image resources from ram
+    unset($page);
+    unset($page_hd);
 }
 
+//Remove duplicates from the array
+$nes_gb_tiles       = array_keys(array_flip($nes_gb_tiles));
+$snes_tiles         = array_keys(array_flip($snes_tiles));
 
+//Now we should have nice arrays of tiles, so let's write them out! We'll do the NES one first
+$max_width          = 2048;
 
+//First, determine the height of the image
+$max_height         = ceil(count($nes_gb_tiles) / ($max_width / 8)) * 8;
+
+//Create a new image to hold this data
+$nes_tile_set       = imagecreate($max_width, $max_height);
+
+//Assign the colors
+$nes_colors     = [
+    imagecolorallocate($nes_tile_set, 0, 0, 0),
+    imagecolorallocate($nes_tile_set, 64, 64, 64),
+    imagecolorallocate($nes_tile_set, 128, 128, 128),
+    imagecolorallocate($nes_tile_set, 192, 192, 192)
+];
+
+imagecolortransparent($nes_tile_set, $nes_colors[0]);
+
+print("Finalizing the NES tileset...\r\n");
+
+//Write the NES tile data
+$x_cell     = 0;
+$y_cell     = 0;
+
+foreach ($nes_gb_tiles as $tile)
+{
+    draw_hex_sprite($nes_tile_set, $nes_colors, $tile, $x_cell, $y_cell);
+    $x_cell     += 8;
+
+    if ($x_cell >= $max_width)
+    {
+        $x_cell     = 0;
+        $y_cell     += 8;
+        
+        //Progress indicator
+        echo chr(27) . "[0G";
+        $percentage         = round(($y_cell / $max_height) * 10);
+        echo '[' . str_repeat('=', $percentage) . str_repeat(' ', 10 - $percentage) . '] ' . ($percentage * 10) . '%';
+    }
+}
+
+//Finally, write the NES image
+imagepng($nes_tile_set, './nes.png');
+unset($nes_tile_set);
+
+//Now, let's do the SNES one
+
+//First, determine the height of the image
+$max_height         = ceil(count($snes_tiles) / ($max_width / 8)) * 8;
+
+//Create a new image to hold this data
+$snes_tile_set       = imagecreate($max_width, $max_height);
+
+//Assign the colors
+$snes_colors     = [
+    imagecolorallocate($snes_tile_set, 0, 0, 0),
+    imagecolorallocate($snes_tile_set, 16, 16, 16),
+    imagecolorallocate($snes_tile_set, 32, 32, 32),
+    imagecolorallocate($snes_tile_set, 48, 48, 48),
+    imagecolorallocate($snes_tile_set, 64, 64, 64),
+    imagecolorallocate($snes_tile_set, 80, 80, 80),
+    imagecolorallocate($snes_tile_set, 96, 96, 96),
+    imagecolorallocate($snes_tile_set, 112, 112, 112),
+    imagecolorallocate($snes_tile_set, 128, 128, 128),
+    imagecolorallocate($snes_tile_set, 144, 144, 144),
+    imagecolorallocate($snes_tile_set, 160, 160, 160),
+    imagecolorallocate($snes_tile_set, 176, 176, 176),
+    imagecolorallocate($snes_tile_set, 192, 192, 192),
+    imagecolorallocate($snes_tile_set, 208, 208, 208),
+    imagecolorallocate($snes_tile_set, 224, 224, 224),
+    imagecolorallocate($snes_tile_set, 240, 240, 240),
+];
+
+imagecolortransparent($snes_tile_set, $snes_colors[0]);
+
+print("\r\n");
+print("Finalizing the SNES tileset...\r\n");
+
+//Write the NES tile data
+$x_cell     = 0;
+$y_cell     = 0;
+
+foreach ($snes_tiles as $tile)
+{
+    draw_hex_sprite($snes_tile_set , $snes_colors, $tile, $x_cell, $y_cell);
+    $x_cell     += 8;
+
+    if ($x_cell >= $max_width)
+    {
+        $x_cell     = 0;
+        $y_cell     += 8;
+        
+        //Progress indicator
+        echo chr(27) . "[0G";
+        $percentage         = round(($y_cell / $max_height) * 10);
+        echo '[' . str_repeat('=', $percentage) . str_repeat(' ', 10 - $percentage) . '] ' . ($percentage * 10) . '%';
+    }
+}
+
+//Finally, write the NES image
+imagepng($snes_tile_set, './snes.png');
+unset($snes_tile_set);
+
+print("\r\n");
+print("Done.\r\n");
 
 ?>
